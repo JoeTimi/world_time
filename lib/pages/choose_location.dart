cd0c7a64c4e05cf0f94a6e810c72afa1cea80d41
Sir Joe <37442841+JoeTimi@users.noreply.github.com>
2022-09-02 22:38:24 +0100

    Initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() {
+
+    // simulate network request for a username
+    Future.delayed(Duration(seconds: 3), () {
+      print('Sir Joe');
+    });
+
+    print('statement');
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
